15dc889b118ac50cc1ebdcbc3295f9fc6899a5f6
Herman Lin <herman.lin236@gmail.com>
2025-07-14 16:49:11 -0400

    add problem 525, WIP

---
+++ 525/solution.py
@@ -0,0 +1,19 @@
+from typing import List
+
+class Solution:
+    def findMaxLength(self, nums: List[int]) -> int:
+        return 0
+    
+sol = Solution()
+sol.findMaxLength([0,1])
+sol.findMaxLength([0,1,0])
+sol.findMaxLength([0,1,1,1,1,1,0,0,0])
+sol.findMaxLength([0, 1, 1, 1, 1, 1, 0, 1, 0, 1])
+sol.findMaxLength([1, 1, 0, 0, 1, 1, 0, 1, 0, 1])
+sol.findMaxLength([0, 0, 0, 1, 1, 0, 0, 1, 1, 0])
+sol.findMaxLength([1, 0, 1, 1, 0, 1, 1, 1, 0, 1])
+sol.findMaxLength([0, 0, 0, 1, 1, 0, 1, 1, 1, 1])
+sol.findMaxLength([1, 0, 0, 0, 1, 1, 0, 1, 1, 0])
+sol.findMaxLength([0, 0, 1, 0, 0, 1, 1, 1, 0, 1])
+sol.findMaxLength([0, 1, 0, 1, 0, 0, 1, 0, 0, 0])
+sol.findMaxLength([1, 1, 1, 1, 1, 1, 1, 1])
